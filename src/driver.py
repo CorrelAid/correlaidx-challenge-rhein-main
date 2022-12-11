a324c60c96656c535ebed5a2a3bc1a41fb15c0c8
lauberto <andrea.grillandi@live.it>
2022-12-11 21:22:26 +0100

    Add some queries

--- src/driver.py
+++ src/driver.py
@@ -1,25 +1,41 @@
-from neo4j import GraphDatabase, basic_auth
+from neo4j import GraphDatabase
 from neo4jconfig import neo4j_config as cfg
 
-uri = cfg["uri"]
-user = cfg["user"]
-password = cfg["password"]
-db = cfg["db"]
+URI = cfg["uri"]
+USER = cfg["user"]
+PASSWORD = cfg["password"]
+DB = cfg["db"]
 
 
 class Driver:
     def __init__(self):
-        self.__uri = uri
-        self.__user = user
-        self.__password = password
-        self.__db = db
+        self.__uri = URI
+        self.__user = USER
+        self.__password = PASSWORD
+        self.__db = DB
 
         self.driver = GraphDatabase.driver(
             self.__uri,
             auth=(self.__user, self.__password)
         )
 
+    def _do_cypher_tx(self, tx, cypher, **cypher_kwargs):
+        result = tx.run(cypher, **cypher_kwargs)
+        values = [record.values() for record in result]
+        return values
+
+    def do_cypher_tx(self, cypher, **cypher_kwargs):
+        with self.driver.session() as session:
+            values = session.read_transaction(
+                self._do_cypher_tx,
+                cypher,
+                **cypher_kwargs
+            )
+        return values
+
     def close(self):
         if self.driver:
             self.driver.close()
 
+
+driver = Driver()
--- src/queries.py
+++ src/queries.py
@@ -1,3 +1,87 @@
-from driver import A
+"""Cypher queries here."""
+from itertools import product
+from argparse import ArgumentParser
 
-print(A)
+from driver import driver
+from edits import edits1, edits2
+
+def contains_lower_name(**params):
+	""" String match in node name."""
+	cypher = """
+	MATCH (n)
+	WHERE toLower(n.name) contains $name
+	RETURN n
+	"""
+	name = params.get('name')
+	if name:
+		name = name.lower()
+		values = driver.do_cypher_tx(cypher, name=name)
+		return values
+	raise ValueError("You shold pass the param 'name'")
+
+def contains_lower_with_edits_name(**params):
+	all_values = {}
+
+	name = params.get('name')
+	name_parts = name.split()
+	edits = [list(edits1(part)) for part in name_parts]
+	for edit in product(*edits):
+		name = " ".join(edit)
+		print(f"Looking for '{name}' on the database, wait a moment...")
+		values = contains_lower_name(name=name)
+		for value in values:
+			element_id = value[0].element_id
+			if not element_id in all_values:
+				all_values[element_id] = value
+	return all_values
+
+def contains_lower_address(**params):
+	""" String match in address name"""
+	cypher = """
+	MATCH (addr:Address)
+	WHERE toLower(addr.name) contains $address
+	RETURN addr
+	"""
+	address = params.get('address')
+	if address:
+		address = address.lower()
+		values = driver.do_cypher_tx(cypher, address=address)
+		return values
+	raise ValueError("You should pass the param 'address'")
+
+
+def _parse_args():
+	parser = ArgumentParser()
+	parser.add_argument("queryname", help="Which query to execute.")
+	parser.add_argument("--name")
+	parser.add_argument("--address")
+	args = parser.parse_args()
+	return vars(args)
+
+if __name__ == "__main__":
+	queryname2func_args = {
+		'contains_lower_name': {
+			'func': contains_lower_name,
+			'args': ['name']
+		},
+		'contains_lower_address': {
+			'func': contains_lower_address,
+			'args': ['address']
+		},
+		'contains_lower_with_edits_name': {
+			'func': contains_lower_with_edits_name,
+			'args': ['name']
+		}
+	}
+
+	args = _parse_args()
+	queryname = args["queryname"]
+	func_queryargs = queryname2func_args.get(queryname)
+	func = func_queryargs.get("func")
+	queryargs = func_queryargs.get("args")
+
+	if func and queryargs:
+		values = [args.get(queryarg) for queryarg in queryargs]
+		queryargs = dict(zip(queryargs, values))
+		values = func(**queryargs)
+		print(values)
